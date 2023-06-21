7787499ba9be9d527c6360327b91ac8fa684a4f9
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2023-06-21 14:45:42 +0200

    Update the Groovy script about the initialization of the JCR provided by Apache Jackrabbit Oak. The script takes care of threes contexts: - an Apache Jackrabbit 2 JCR already exists: it does a backup of it and   modifies the configuration files to be ready for a migration to Apache   Jackrabbit Oak with the dedicated tool provided by Oak. It creates the   JCR home directory for Oak. The index definitions for Silverpeas   should be created during the migration process by using our own   distribution of the Apache Jackrabbit Oak migration tool. - First installation: the JCR home directory and the Apache Jackrabbit   Oak is created and initialized with index definitions for Silverpeas. - No fresh installation and a non Apache Jackrabbir 2 JCR already   exists: it assumes the JCR is managed by Oak. Browse the index   definitions in the silverpeas-oak-index.properties and for each of   them, if the definition isn't already created, creates it.
    Whatever the context, the script assumes by default the content storage
    of the Apache Jackrabbit Oak JCR is on the filesystem (storage = segment
    in the silverpeas-oak.properties). For another storage and for an
    upgrade from an Apache Jackrabbit 2 JCR or a first installation, don't
    forget to update the property storage in
    SILVERPEAS_HOME/configuration/silverpeas/resource/silverpeas-oak.properties.
    
    The index definitions to create are all declared in the properties file
    SILVERPEAS_HOME/configuration/silverpeas/resource/silverpeas-oak-index.properties.

--- src/main/dist/configuration/silverpeas/01-jcrSettings.groovy
+++ src/main/dist/configuration/silverpeas/01-jcrSettings.groovy
@@ -5,19 +5,75 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
 
+import org.apache.jackrabbit.commons.cnd.CndImporter
+import org.apache.jackrabbit.oak.plugins.index.IndexUtils
+import org.apache.jackrabbit.oak.spi.state.NodeBuilder
+
 /**
- * This script setups the JCR repository configuration file according to the customer configuration
- * properties. If a previous JCR managed by Jackrabbit is found, it is before renamed and then the
- * repository.xml and workspace.xml configuration files are updated for an eventual migration to
- * Oak.
+ * The JCR is now provided by Apache Jackrabbit Oak and not anymore by Apache Jackrabbit 2.
+ * Apache Jackrabbit Oak is a more suitable solution for enterprise class environment. Two
+ * storage backends are available for the JCR with Apache Jackrabbit Oak: either the filesystem for
+ * vertical high-scalability or MongoDB for horizontal high-scalability.
+ * <p>
+ * This script setups the JCR repository configuration file according to the configuration properties
+ * defined in the template SILVERPEAS_HOME/silverpeas/resources/silverpeas-oak.properties. If this is
+ * a first installation of Silverpeas, to custom the configuration of the JCR, mainly the storage to use
+ * by Oak, please update the template before any execution of the silverpeas configuration task because the
+ * index definitions are created during the configuration phase of the Silverpeas installation.
+ * <p>
+ * This script takes in charge three contexts of the installation: a first installation, an upgrade from a previous
+ * version in which the JCR was managed by Apache Jackrabbit 2, and an upgrade from a previous version in which the
+ * JCR is already managed by Apache Jackrabbit Oak:
+ * <ul>
+ * <li>
+ * In the case of a first installation, the JCR is created in the JCR home directory and the indexation is initialized
+ * from the index configuration file SILVERPEAS_HOME/silverpeas/resources/silverpeas-oak-index.properties.
+ * <li>
+ * If a previous JCR managed by Jackrabbit 2 is found, the JCR home directory is before renamed to jackrabbit
+ * and then the repository.xml and workspace.xml configuration files are updated for an eventual migration
+ * to Oak. Don't forget to migrate the JCR content from Apache Jackrabbit 2 to the Apache Jackrabbit Oak
+ * before starting Silverpeas in order to retrieve your data. Unlike a first installation, the indexation is
+ * initialized by the migration process itself. For doing, please contact the Silverpeas team.
+ * <li>
+ * If the JCR is already managed by Oak, then the index definitions is updated from the index configuration file
+ * SILVERPEAS_HOME/silverpeas/resources/silverpeas-oak.properties. In the case there are some new index definitions,
+ * it is required to use the tool oak-run to reindex all the content of the JCR according to the new index definitions.
+ * </ul>
  * @author mmoquillon
  */
 
-log.info 'Configure the JCR repository'
+// method to create a definition of an index on the given properties of JCR nodes
+void createIndexDefinitionOnProperty(NodeBuilder indexRoot, String indexName, String... propertyName) {
+  if (!indexRoot.hasChildNode(indexName)) {
+    // additional security to ensure we don't create again the same index definitions
+    String prop = propertyName.length > 1 ? 'properties' : 'property'
+    log.info "Create index ${indexName} on ${prop} ${String.join(' and ', propertyName)}"
+    IndexUtils.createIndexDefinition(indexRoot, indexName, true, false,
+        Arrays.asList(propertyName),
+        List.of('slv:simpleDocument'));
+  }
+}
+
+void createWholeIndexDefinitions(Map jcrParams) {
+  Path jcrIndexDefinitionsPath =
+      "${settings.CONFIGURATION_HOME}/silverpeas/resources/silverpeas-oak-index.properties".asPath()
+  Properties indexDefinitions = new Properties()
+  indexDefinitions.load(Files.newBufferedReader(jcrIndexDefinitionsPath))
+  jcr.performOnNodeRootState(jcrParams, { root ->
+    NodeBuilder index = IndexUtils.getOrCreateOakIndex(root);
+    indexDefinitions.each {definition ->
+      String[] np = definition.value.split(' ').collect { it.trim() }
+      createIndexDefinitionOnProperty(index, definition.key, np);
+    }
+  })
+}
+
+log.info 'Configure the JCR repository...'
 
 Path jcrHomePath = settings.JCR_HOME.asPath()
 Path jcrConfigurationPath = "${settings.JCR_HOME}/silverpeas-oak.properties".asPath()
 Path jcrConfigurationTemplatePath = "${settings.CONFIGURATION_HOME}/silverpeas/resources/silverpeas-oak.properties".asPath()
+Map jcrParams = ['JCR_HOME': jcrHomePath.toString()]
 
 /* case of a jcr home directory whose the repository is managed by jackrabbit */
 Path jcrConfig = "${settings.JCR_HOME}/repository.xml".asPath()
@@ -27,7 +83,7 @@ if (Files.exists(jcrConfig)) {
 
   Files.move(jcrHomePath, Path.of(jcrHomePath.parent.toString(), 'jackrabbit'))
 
-  log.info 'Backup the Jackrabbit 2 configuration files and update them for the JCR migration...'
+  log.info 'Backup the Jackrabbit 2 configuration files and update them for the JCR migration to Oak...'
 
   String jcrUrl = (settings.JCR_URL ? settings.JCR_URL : settings.DB_URL).toString()
   String jcrUser = (settings.JCR_USER ? settings.JCR_USER : settings.DB_USER).toString()
@@ -93,13 +149,32 @@ if (Files.exists(jcrConfig)) {
     namespaces['.empty.key']=''
     namespaces.store(new FileOutputStream(namespacesPath.toFile()), null)
   }
-}
 
-/* creates the JCR home directory if it doesn't already exist and copies the JCR configuration file
-* into this directory */
-service.createDirectory(jcrHomePath, [readable: true, writable: true, executable: true])
-if (!Files.exists(jcrConfigurationPath)) {
+  // create the JCR home directory and puts into it the JCR configuration file
+  log.info 'Create the JCR home directory for Oak and the JCR configuration file from the template...'
+  service.createDirectory(jcrHomePath, [readable: true, writable: true, executable: true])
   Files.copy(jcrConfigurationTemplatePath, jcrConfigurationPath)
+
+} /* case of a first installation of Silverpeas */
+else if (! Files.exists(jcrConfigurationPath)) {
+  // create the JCR home directory and puts into it the JCR configuration file
+  log.info 'Create the JCR home directory and the JCR configuration file from the template...'
+  service.createDirectory(jcrHomePath, [readable: true, writable: true, executable: true])
+  Files.copy(jcrConfigurationTemplatePath, jcrConfigurationPath)
+
+  // create the JCR schema for Silverpeas
+  log.info 'Register the Silverpeas schema into the JCR...'
+  Path jcrSilverpeasSchemaPath = "${settings.CONFIGURATION_HOME}/silverpeas/resources/silverpeas-jcr.cnd".asPath()
+  Files.newBufferedReader(jcrSilverpeasSchemaPath).withCloseable { reader ->
+    jcr.openSession(jcrParams).withCloseable { session ->
+      CndImporter.registerNodeTypes(reader, session);
+    }
+  }
+
+  log.info 'Create the index definitions for Silverpeas into the JCR...'
+  createWholeIndexDefinitions(jcrParams)
+} else {
+  createWholeIndexDefinitions(jcrParams)
 }
 
 
